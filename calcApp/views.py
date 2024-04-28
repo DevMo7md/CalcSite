ccf28877c0a8d5fd6d4d7ff0390b7ba40a669790
Mohammed Younis <156939965+DevMo7md@users.noreply.github.com>
2024-04-29 00:05:37 +0200

    version 1.0.0

---
+++ calcApp/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+# Create your views here.
+def main (request):
+    context ={}
+    return render(request, 'home.html', context=context)
+
+
+def home(request):
+    if request.method == "POST":
+        snum = request.POST.get('snum')
+        lnum = request.POST.get('lnum')
+        opr = request.POST.get('opr')
+        if opr == "plus":
+            result = float(snum) + float(lnum)
+            
+        elif opr == 'minus' :
+            result = float(snum) - float(lnum)
+            context = {'result': result}
+            return render(request, 'home.html', context)
+            
+        elif opr == 'multiply' :
+            result = float(snum) * float(lnum)
+            context = {'result': result}
+            return render(request, 'home.html', context)
+            
+        elif opr == 'devide' :
+            result = float(snum) / float(lnum)
+            context = {'result': result}
+            return render(request, 'home.html', context)
+            
+        else:
+            return render(request,'home.html')
+        
+        context = {'result': result}
+        return render(request, 'home.html', context)
+    return redirect("/")    
